0d3ccbc725380b0c01bf735de0e664b9feaac34a
Alexandre Rocha <avrocha99@gmail.com>
2020-11-02 23:24:48 +0000

    updated config files; beta version follow_wall.py

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # wall-following-2d-robot
 ROBO@FEUP - 2nd Assignment - Wall Following 2D Robot using ROS and STDR simulator
 
+# STDR Simulator Configuration
+Copy all the contents of 'stdr_resources/maps' to the respective folder inside the stdr_resources package (use 'roscd stdr_resources/maps')
+Copy all the contents of 'stdr_resources/resources/robots' to the respective folder in the stdr_resources package (use 'roscd stdr_resources/resources/robots')
+
 # Build
+1)
+```shell
+$ cd ~/catkin_ws/src/wall_following_2d_robot/src
+$ chmod +x follow_wall.py
+```
+2)
+```shell
+$ cd ~/catkin_ws
+$ catkin_make
+```
 
-# Compile
+# Run
+```shell
+'roslaunch stdr_launchers wall_follow_robot.launch'
+```
 
-# Dependencies
+```shell
+'rosrun wall_following_2d_robot follow_wall.py'
+```
--- src/wall_following_2d_robot/src/follow_wall.py
+++ src/wall_following_2d_robot/src/follow_wall.py
@@ -4,21 +4,33 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-def callback_laser(msg):
+def callback_laser(data):
+    print('first beam value :', data.ranges[0])
+
 
 def follow_wall():
+    msg.linear.x = 1
+    msg.angular.z = 1
 
 def main():
-    rospy.init_node('Read LIDAR')
+    rospy.init_node('ReadLIDAR')
 
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-    sub = rospy.Subscriber('[robot]/laser/scan', LaserScan, callback_laser)
+    global msg
 
-    while not rospy.is_shutdown():
-        follow_wall()
+    pub = rospy.Publisher('robot0/cmd_vel', Twist, queue_size=10)
+    sub = rospy.Subscriber('robot0/laser0', LaserScan, callback_laser)
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():      
         msg = Twist()
+        follow_wall()
+        pub.publish(msg)
 
-        
+        rate.sleep()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
